58cd494c44b0b5c32e7db117431554b2e9b3883c
KedrickHill <kedrickjhill@gmail.com>
2021-03-08 16:35:05 -0600

    Added annotations and request redirects based on user input for email and password

--- registration/src/main/java/com/init/VerificationInit.java
+++ registration/src/main/java/com/init/VerificationInit.java
@@ -1,10 +1,21 @@
 package com.init;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.verify.Credentials;
+import com.verify.VerificationUtil;
 
 /**
  * Servlet implementation class VerificationInit
@@ -24,16 +35,72 @@ public class VerificationInit extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		try {
+			PrintWriter out = response .getWriter();
+			out.println("<html><body>");
+			
+			SessionFactory factory = VerificationUtil.getSessionFactory();
+			Session session = factory.openSession();
+			
+			out.println("This is the doGet method");
+			
+			session.close();
+		} catch (Exception e) {
+			throw e;
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		try {
+			PrintWriter out = response.getWriter();
+			
+			String email = request.getParameter("email");
+			String pswd = request.getParameter("pswd");
+			
+			SessionFactory factory = VerificationUtil.getSessionFactory();
+			Session session = factory.openSession();
+			
+			List<Credentials> creds = session.createQuery("from com.verify.Credentials", Credentials.class).list();
+			
+			out.println("<html><body> Checking Credentails...");
+		
+			Optional<Credentials> user = creds.stream()
+					.filter( c -> c.getEmail().equals(email))
+					.findFirst();
+			
+			if (user.isPresent()) {
+				Credentials c = user.get();
+				if(!c.getPassword().equals(pswd)) {
+					request.setAttribute("errorMessage", "Invalid Password");
+					request.getRequestDispatcher("/index.jsp").forward(request, response);
+				}
+				else {
+					request.setAttribute("name", c.getFirstName() + c.getLastName());
+					request.getRequestDispatcher("/Welcome").forward(request, response);
+
+				}
+//				RequestDispatcher reqDispatcher = request.getRequestDispatcher("/welcome.jsp");
+//				reqDispatcher.forward(request, response);
+//				response.sendRedirect("/Welcome");
+			}
+			else {
+				request.setAttribute("errorMessage", "Not a Valid Email");
+				request.getRequestDispatcher("/index.jsp").forward(request, response);
+			}
+			
+			
+				
+				out.println("</body></html>");
+				session.close();
+
+		} catch (Exception e) {
+			throw e;
+		}
 	}
 
 }
--- registration/src/main/webapp/index.jsp
+++ registration/src/main/webapp/index.jsp
@@ -1,7 +1,7 @@
 <html>
 <body>
 <h2>Credential Verification</h2>
-
+<div style="color: #FF0000;">${errorMessage}</div>
 <form action="Logged-In" method="post">
 	Email: <input name="email" type="text" id="email"/><br/>  
 	Password: <input name="password" type="text" id="pswd"/><br/>  
